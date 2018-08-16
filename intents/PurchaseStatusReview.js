e48769b604543a4073b6b18b9af48367ac82bc8d
Zach Cannon <zcannon@rtsfinancial.com>
2018-08-16 13:44:44 -0500

    Added Intents Folder and Stub

---
+++ intents/PurchaseStatusReview.js
@@ -0,0 +1,14 @@
+const PurchaseStatusReviewIntent = {
+    canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && handlerInput.requestEnvelope.request.intent.name === 'PURCHASE';
+    },
+    handle(handlerInput) {
+      const speechText = 'Hello World!';
+  
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .withSimpleCard('Hello World', speechText)
+        .getResponse();
+    },
+  };
